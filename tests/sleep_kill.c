4c4257216545bfbe1b42417ec64735e8ce4709c5
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-02-03 15:41:49 -0500

    sleep working

--- tests/sleep_kill.c
+++ tests/sleep_kill.c
@@ -1,14 +1,7 @@
 #include "common.h"
 
 int main() {
-  struct timespec sleep_ts = {
-    .tv_sec = 60,
-    .tv_nsec = 0
-  };
   PRINT_INT("PID", getpid());
-  struct timespec rem_ts = {0};
-
-  nanosleep(&sleep_ts, &rem_ts);
-
+  sleep(60);
   return 1;
 }
