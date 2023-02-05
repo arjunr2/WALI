34da04609e2357aff9a49ef8d9780905eeedf279
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-02-05 16:50:14 -0500

    Added alarm test and startup gitignore

---
+++ tests/alarm.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+#define ALARM_TIME 3
+
+int main() {
+  pid_t cpid = syscall(SYS_fork);
+  int ct = 0;
+  /* Child */
+  if (cpid == 0) {
+    syscall(SYS_alarm, ALARM_TIME);
+    while (1) {
+      printf("Child ct: %d\n", ct);
+      ct++;
+      sleep(1);
+    }
+  }
+  /* Parent */
+  else {
+    int i = 0;
+    for (int i = 0; i < ALARM_TIME + 3; i++) {
+      printf("Parent ct: %d\n", ct);
+      ct++;
+      sleep(1);
+    }
+  }
+  return 0;
+}
