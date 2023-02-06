6ef0a9601e4924fd0be5c1c0ed57d2a532b0c8c9
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-02-05 19:05:21 -0500

    Added sigprocmask, f/statfs tests

---
+++ tests/fstatfs.c
@@ -0,0 +1,27 @@
+#include "common.h"
+#include <sys/vfs.h>
+
+int main() {
+  struct statfs sb;
+  int fd = open("compile-wali.sh", O_RDONLY);
+  if (fstatfs(fd, &sb)) {
+    printf("Error\n");
+    return 1;
+  }
+  printf("Size of statfs: %d\n", sizeof(sb));
+
+  printf("F-type: %lu\n", sb.f_type);
+  printf("F-bsize: %lu\n", sb.f_bsize);
+  printf("F-blocks: %lu\n", sb.f_blocks);
+  printf("F-bfree: %lu\n", sb.f_bfree);
+  printf("F-bavail: %lu\n", sb.f_bavail);
+
+  printf("F-files: %lu\n", sb.f_files);
+  printf("F-ffree: %lu\n", sb.f_ffree);
+  printf("F-fsid: %u | %u\n", sb.f_fsid.__val[0], sb.f_fsid.__val[1]);
+  printf("F-namelen: %lu\n", sb.f_namelen);
+  printf("F-frsize: %lu\n", sb.f_frsize);
+  printf("F-flags: %lu\n", sb.f_flags);
+
+  return 0;
+}
---
+++ tests/statfs.c
@@ -0,0 +1,26 @@
+#include "common.h"
+#include <sys/vfs.h>
+
+int main() {
+  struct statfs sb;
+  if (statfs("compile-wali.sh", &sb)) {
+    printf("Error\n");
+    return 1;
+  }
+  printf("Size of statfs: %d\n", sizeof(sb));
+
+  printf("F-type: %lu\n", sb.f_type);
+  printf("F-bsize: %lu\n", sb.f_bsize);
+  printf("F-blocks: %lu\n", sb.f_blocks);
+  printf("F-bfree: %lu\n", sb.f_bfree);
+  printf("F-bavail: %lu\n", sb.f_bavail);
+
+  printf("F-files: %lu\n", sb.f_files);
+  printf("F-ffree: %lu\n", sb.f_ffree);
+  printf("F-fsid: %u | %u\n", sb.f_fsid.__val[0], sb.f_fsid.__val[1]);
+  printf("F-namelen: %lu\n", sb.f_namelen);
+  printf("F-frsize: %lu\n", sb.f_frsize);
+  printf("F-flags: %lu\n", sb.f_flags);
+
+  return 0;
+}
