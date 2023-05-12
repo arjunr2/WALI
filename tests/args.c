f405ee25a3528a0cb0dbda84da0f0a52242b866e
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-05-12 15:47:57 -0400

    Added args test

--- tests/args.c
+++ tests/args.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     printf("Arg %d: %s\n", i, argv[i]);
   }
