8494879aa7b4c10bf5d56d76212f74aefde9c2d7
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-06-22 17:18:04 -0400

    Added example for test applications

---
+++ wasm-apps/bash/k.sh
@@ -0,0 +1,6 @@
+#Print the prompt message
+echo "Enter your favorite color: "  
+#Take the input
+read  
+#Print the input value
+echo "Your favorite color is $REPLY"
---
+++ wasm-apps/sqlite/list_albums.sql
@@ -0,0 +1,4 @@
+SELECT albumid, title
+FROM albums
+ORDER BY title
+LIMIT 10;
