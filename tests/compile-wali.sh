8d8aaf060986fae1dd298629f22c4a23d6629bc7
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-12-01 12:38:15 -0500

    Change test run format

--- tests/compile-wali.sh
+++ tests/compile-wali.sh
@@ -23,7 +23,7 @@ outbase=$outdir/$(basename $cfile .c)
 
 crtfile=$sysroot_dir/lib/crt1.o 
 
-$CC $verbose --target=wasm32-wasi-threads -O1 -pthread --sysroot=$sysroot_dir $cfile -c -o $outbase.wasm
-wasm2wat --enable-threads $outbase.wasm -o $outbase.wat
-$LD $verbose --no-gc-sections --no-entry --shared-memory --export-memory --import-memory --max-memory=67108864 --allow-undefined -L$sysroot_dir/lib $outbase.wasm $crtfile -lc -lm $rt_lib -o ${outbase}_link.wasm
-wasm2wat --enable-threads ${outbase}_link.wasm -o ${outbase}_link.wat
+$CC $verbose --target=wasm32-wasi-threads -O1 -pthread --sysroot=$sysroot_dir $cfile -c -o $outbase.int.wasm
+wasm2wat --enable-threads $outbase.int.wasm -o $outbase.int.wat
+$LD $verbose --no-gc-sections --no-entry --shared-memory --export-memory --import-memory --max-memory=67108864 --allow-undefined -L$sysroot_dir/lib $outbase.int.wasm $crtfile -lc -lm $rt_lib -o ${outbase}.wasm
+wasm2wat --enable-threads ${outbase}.wasm -o ${outbase}.wat
