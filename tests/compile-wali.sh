f4e746ff81bee09e4b1d87c4d660c419e8a922d7
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-12-14 09:21:13 -0500

    Remove import memory test build flag

--- tests/compile-wali.sh
+++ tests/compile-wali.sh
@@ -25,5 +25,5 @@ crtfile=$sysroot_dir/lib/crt1.o
 
 $CC $verbose --target=wasm32-wasi-threads -O1 -pthread --sysroot=$sysroot_dir $cfile -c -o $outbase.int.wasm
 wasm2wat --enable-threads $outbase.int.wasm -o $outbase.int.wat
-$LD $verbose --no-gc-sections --no-entry --shared-memory --export-memory --import-memory --max-memory=67108864 --allow-undefined -L$sysroot_dir/lib $outbase.int.wasm $crtfile -lc -lm $rt_lib -o ${outbase}.wasm
+$LD $verbose --no-gc-sections --no-entry --shared-memory --export-memory --max-memory=67108864 --allow-undefined -L$sysroot_dir/lib $outbase.int.wasm $crtfile -lc -lm $rt_lib -o ${outbase}.wasm
 wasm2wat --enable-threads ${outbase}.wasm -o ${outbase}.wat
