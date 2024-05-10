fc3a3661b0e39c174589a62ba83812c5996f2f25
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2024-05-10 12:02:49 -0400

    Fix wali_bashenv and test compilation flag

--- tests/compile-wali.sh
+++ tests/compile-wali.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
+source ../wali_bashenv.sh
+
 outdir=.
-LLVM_DIR=../llvm-project
 
-rt_lib=$LLVM_DIR/build/lib/clang/16/lib/wasi/libclang_rt.builtins-wasm32.a
-
-CC=$LLVM_DIR/build/bin/clang
-LD=$LLVM_DIR/build/bin/wasm-ld
+rt_lib=$WALI_DIR/llvm-project/build/lib/clang/16/lib/wasi/libclang_rt.builtins-wasm32.a
 
 while getopts "vo:s:" OPT; do
   case $OPT in
@@ -21,9 +19,9 @@ done
 cfile=${@:$OPTIND:1}
 outbase=$outdir/$(basename $cfile .c)
 
-crtfile=$sysroot_dir/lib/crt1.o 
+crtfile=$WALI_SYSROOT_DIR/lib/crt1.o 
 
-$CC $verbose --target=wasm32-wasi-threads -O1 -pthread --sysroot=$sysroot_dir $cfile -c -o $outbase.int.wasm
+$WALI_CC $verbose $WALI_CFLAGS $cfile -c -o $outbase.int.wasm
 wasm2wat --enable-threads $outbase.int.wasm -o $outbase.int.wat
-$LD $verbose --no-gc-sections --no-entry --shared-memory --export-memory --max-memory=67108864 --allow-undefined -L$sysroot_dir/lib $outbase.int.wasm $crtfile -lc -lm $rt_lib -o ${outbase}.wasm
+$WALI_LD $verbose --no-gc-sections --no-entry --shared-memory --export-memory --max-memory=67108864 --allow-undefined -L$sysroot_dir/lib $outbase.int.wasm $crtfile -lc -lm $rt_lib -o ${outbase}.wasm
 wasm2wat --enable-threads ${outbase}.wasm -o ${outbase}.wat
--- wali_bashenv.sh
+++ wali_bashenv.sh
@@ -3,12 +3,18 @@ export WALI_DIR=$(dirname $(realpath ${BASH_SOURCE}))
 
 WALI_LLVM_BIN=$WALI_DIR/llvm-project/build/bin
 
+# LLVM Compilation tools 
 export WALI_CC=$WALI_LLVM_BIN/clang
 export WALI_CXX=$WALI_LLVM_BIN/clang++
+export WALI_LD=$WALI_LLVM_BIN/wasm-ld
 export WALI_AR=$WALI_LLVM_BIN/llvm-ar
 export WALI_RANLIB=$WALI_LLVM_BIN/llvm-ranlib
 
-export WALI_CFLAGS=="--target=wasm32-wasi-threads -pthread --sysroot=/$WALI_DIR/wali-musl/sysroot -matomics -mbulk-memory -mmutable-globals -msign-ext"
+# Sysroot
+export WALI_SYSROOT_DIR=$WALI_DIR/wali-musl/sysroot
+
+# C/C++ flags
+export WALI_CFLAGS="--target=wasm32-wasi-threads -pthread --sysroot=/$WALI_DIR/wali-musl/sysroot -matomics -mbulk-memory -mmutable-globals -msign-ext"
 export WALI_CXXFLAGS="-stdlib=libc++ --target=wasm32-wasi-threads -pthread --sysroot=/$WALI_DIR/wali-musl/sysroot -I/$WALI_DIR/libcxx/include/c++/v1 -matomics -mbulk-memory -mmutable-globals -msign-ext"
 export WALI_LDFLAGS="-L/$WALI_DIR/wali-musl/sysroot/lib -L/$WALI_DIR/libcxx/lib -Wl,--shared-memory -Wl,--export-memory -Wl,--max-memory=2147483648"
 
