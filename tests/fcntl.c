c17a3f61b96059bf48ec8fa8f2989a97ab3fd545
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2024-06-26 12:47:37 -0400

    [Fix] fcntl lock/ownership flag support

--- tests/fcntl.c
+++ tests/fcntl.c
@@ -5,5 +5,6 @@ int main() {
   int fd = open("random_file", O_RDWR);
   int flags = fcntl(fd, F_GETFL, 0);
   int ret = fcntl(fd, F_SETFL, flags);
+  printf("Struct flock size: %d\n", sizeof(struct flock));
   return 0;
 }
