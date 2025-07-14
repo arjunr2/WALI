d989682445c1a56d62ae3a1a966b68d36861b097
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2025-07-14 14:05:08 -0400

    Fix wali tests script to use bash config variable

--- tests/compile-wali.sh
+++ tests/compile-wali.sh
@@ -10,7 +10,6 @@ while getopts "vo:s:" OPT; do
   case $OPT in
     v) verbose=--verbose;;
     o) outdir=$OPTARG;;
-    s) sysroot_dir=$OPTARG;;
     *) 
       echo "Incorrect opt provided"
       exit 1 ;;
@@ -23,5 +22,5 @@ crtfile=$WALI_SYSROOT_DIR/lib/crt1.o
 
 $WALI_CC $verbose $WALI_CFLAGS $cfile -c -o $outbase.int.wasm
 wasm2wat --enable-threads $outbase.int.wasm -o $outbase.int.wat
-$WALI_LD $verbose --no-gc-sections --no-entry --shared-memory --export-memory --max-memory=67108864 --allow-undefined -L$sysroot_dir/lib $outbase.int.wasm $crtfile -lc -lm $rt_lib -o ${outbase}.wasm
+$WALI_LD $verbose --no-gc-sections --no-entry --shared-memory --export-memory --max-memory=67108864 --allow-undefined -L$WALI_SYSROOT_DIR/lib $outbase.int.wasm $crtfile -lc -lm $rt_lib -o ${outbase}.wasm
 wasm2wat --enable-threads ${outbase}.wasm -o ${outbase}.wat
