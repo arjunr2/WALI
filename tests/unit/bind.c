5029a2b5b6bfa77c6c413722f5b13ecaf3fac86b
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2026-04-27 14:09:30 -0400

    Test Augment: Batch 1

--- tests/unit/bind.c
+++ tests/unit/bind.c
@@ -1,34 +1,82 @@
-// CMD: setup="" args="" cleanup=""
+// CMD: args="loopback"
+// CMD: args="any"
+// CMD: args="wrong_family"
+// CMD: args="unix_abstract"
+// CMD: setup="clean" args="unix_path" cleanup="clean"
 
 #include "wali_start.c"
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <stddef.h>
 
-int test_bind_addr(uint32_t ip_addr, const char* desc) {
-    long fd = wali_syscall_socket(AF_INET, SOCK_DGRAM, 0);
-    TEST_ASSERT_NE(fd, -1);
-    
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(ip_addr);
-    addr.sin_port = 0; // Let OS choose
+#define UNIX_PATH "/tmp/wali_bind_unix.sock"
 
-    if (ip_addr == INADDR_LOOPBACK) {
-        TEST_LOG("Testing bind to loopback");
-    } else {
-        TEST_LOG("Testing bind to any");
+#ifdef WALI_TEST_WRAPPER
+int test_setup(int argc, char **argv) {
+    if (argc >= 1 && strcmp(argv[0], "clean") == 0) {
+        unlink(UNIX_PATH);
     }
-    long res = wali_syscall_bind(fd, (struct sockaddr*)&addr, sizeof(addr));
-    wali_syscall_close(fd);
-    
-    return (res == 0) ? 0 : -1;
+    return 0;
 }
+int test_cleanup(int argc, char **argv) {
+    if (argc >= 1 && strcmp(argv[0], "clean") == 0) {
+        unlink(UNIX_PATH);
+    }
+    return 0;
+}
+#endif
 
 int test(void) {
-    TEST_ASSERT_EQ(test_bind_addr(INADDR_LOOPBACK, "loopback"), 0);
-    TEST_ASSERT_EQ(test_bind_addr(INADDR_ANY, "any"), 0);
-    return 0;
+    if (test_init_args() != 0) return -1;
+    const char *mode = (argc > 1) ? argv[1] : "loopback";
+
+    int domain = (strncmp(mode, "unix", 4) == 0) ? AF_UNIX : AF_INET;
+    int fd = wali_syscall_socket(domain, SOCK_DGRAM, 0);
+    TEST_ASSERT(fd >= 0);
+
+    long ret;
+    int expect_ok = 1;
+
+    if (strcmp(mode, "loopback") == 0 || strcmp(mode, "any") == 0) {
+        struct sockaddr_in addr;
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = htonl(strcmp(mode, "loopback") == 0 ? INADDR_LOOPBACK : INADDR_ANY);
+        addr.sin_port = 0;
+        ret = wali_syscall_bind(fd, &addr, sizeof(addr));
+    } else if (strcmp(mode, "wrong_family") == 0) {
+        struct sockaddr_in addr;
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family = AF_INET6;
+        addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+        addr.sin_port = 0;
+        ret = wali_syscall_bind(fd, &addr, sizeof(addr));
+        expect_ok = 0;
+    } else if (strcmp(mode, "unix_abstract") == 0) {
+        struct sockaddr_un addr;
+        memset(&addr, 0, sizeof(addr));
+        addr.sun_family = AF_UNIX;
+        const char *name = "wali_bind_test_abstract";
+        size_t nlen = strlen(name);
+        addr.sun_path[0] = '\0';
+        memcpy(&addr.sun_path[1], name, nlen);
+        socklen_t alen = (socklen_t)(offsetof(struct sockaddr_un, sun_path) + 1 + nlen);
+        ret = wali_syscall_bind(fd, &addr, alen);
+    } else if (strcmp(mode, "unix_path") == 0) {
+        struct sockaddr_un addr;
+        memset(&addr, 0, sizeof(addr));
+        addr.sun_family = AF_UNIX;
+        strncpy(addr.sun_path, UNIX_PATH, sizeof(addr.sun_path) - 1);
+        ret = wali_syscall_bind(fd, &addr, sizeof(addr));
+    } else {
+        wali_syscall_close(fd);
+        return -1;
+    }
+
+    wali_syscall_close(fd);
+    int success = (ret == 0);
+    return (success == expect_ok) ? 0 : -1;
 }
